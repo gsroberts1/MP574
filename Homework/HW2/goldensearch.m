3f7ded12392e1a47b93cf6553319b0976b46316a
gsroberts1 <gsroberts@wisc.edu>
2019-02-20 17:43:43 -0600

    Wed, Feb 20, 2019  5:43:42 PM

--- Homework/HW2/goldensearch.m
+++ Homework/HW2/goldensearch.m
@@ -1,27 +1,25 @@
-function [alpha] = goldensearch(f,df,x,a0,b0,iters)
-%   goldensearch - provides a golden section search of a 1d function
-%   a0 and b0: defines the interval search area.
-%   iters: number of iterations to perform
-%   fun: 1D function to perform search.
-%   guess: define starting
-%   estim: final estimator output
-%   err: difference between the two eval points (absolute)
-%   golden value is rho = 0.382
-rho = 0.382;
-tol = 0.0001;
-h = @(alpha) f(x-alpha*df(x));
+function [alpha] = goldenSearch(f,df,x,iters)
+%%   goldenSearch
+%    Finds 1D mimimum of function along gradient direction
+
+a0 = 0; % lower bound on golden section search
+b0 = 1; % upper bound on golden section search
+rho = 0.382; % golden value
+tol = 0.0001; % tolerance
+h = @(alpha) f(x-alpha*df(x)); %golden section search 
+
 for ii = 1:iters
-    a1 = rho*(b0 - a0)+ a0;
-    b1 = b0-rho*(b0 - a0);
+    a1 = rho*(b0 - a0)+ a0; 
+    b1 = b0-rho*(b0 - a0);  
     if abs(h(a1)-h(b1)) < tol
-        alpha = a1;
-        break
+        alpha = a1; % f(a1) and f(b1) are very close
+        break % leave loop, we've found the minimum
     elseif h(a1) >= h(b1)
         a0 = a1;
     elseif h(a1) < h(b1)
         b0 = b1;
     else 
-        alpha = a1;
+        alpha = a1; %lets go home
     end
 end
 
